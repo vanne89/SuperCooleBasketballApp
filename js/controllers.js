810eedf8081a99f4b8ecc424cb8b3f458ecf26c2
vanne89 <anne.vlaanderen@gmail.com>
2013-11-13 14:54:01 +0100

    Sorted out the json call. Made a first table with games
    Json works. Parses json ta a workable table.  Small adjusments to the
    layout and design.

--- js/controllers.js
+++ js/controllers.js
@@ -1,38 +1,42 @@
 var scoreApp = angular.module('scoreApp', []);
  
-scoreApp.controller('ScheduleListController', function ScheduleListController($scope) {
+scoreApp.controller('ScheduleListController', function ScheduleListController($scope, $http) {
  
 	var accessToken = '82996312dc';
 	var tournamentID = '19389';
-
-
-	$scope.loadGames = function(){
-		console.log("functie is bereikt");
-		
-
-
-		 var jsonRequestURL = 'https://api.leaguevine.com/v1/games/?tournament_id='+ tournamentID +'&access_token=' + accessToken;
-		 console.log(jsonRequestURL);
-		 $http.get(jsonRequestURL).then(function(response) {
-      	console.log(response);
-         });
-	};
 	
-	$scope.config(function($routeProvider) {
-		$routeProvider.when('/games', {
-			templateUrl: 'games.html',
-			controller: 'LoginController'
-		});
+	$scope.games = [];
 
-		$routeProvider.otherwise({ redirectTo : '/login'});
-	});
+ 
+	var jsonRequestURL = 'https://api.leaguevine.com/v1/games/?tournament_id='+ tournamentID +'&access_token=' + accessToken;
+	console.log(jsonRequestURL);
+$http({method: 'GET', url: jsonRequestURL}).
+  success(function(data, status, headers, config) {
+ console.log(data.objects);
+ var strippedData = data.objects;
+for (var i = 0; i < strippedData.length; i++) {
+	var game = [];
+	game.date = strippedData[i].start_time;
+	game.team_1 = strippedData[i].team_1.name;
+	game.team_2 = strippedData[i].team_2.name;
 
-	$scope.controller('LoginController', function(){
 
-	});
+	console.log(game.date + "  " + game.team_1 + " vs. " + game.team_2);
+
+	$scope.games.push(game);
+};
+ console.log(games);
+
+
+  }).
+  error(function(data, status, headers, config) {
+    // called asynchronously if an error occurs
+    // or server returns response with an error status.
+  });
 
 
 
-	$scope.naam = "Bastiaan en Anne";
+
+
 });
 
