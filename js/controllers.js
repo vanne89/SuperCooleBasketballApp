44ff4ca01c78b8f6d84f83e69468ba454a594f5a
vanne89 <anne.vlaanderen@gmail.com>
2013-11-14 15:37:19 +0100

    Routing solved
    Routing solved, lost data. Only works in Mozilla somehow.

--- js/controllers.js
+++ js/controllers.js
@@ -1,7 +1,88 @@
 var scoreApp = angular.module('scoreApp', []).
 	config(function($routeProvider){
-		$routeProvider.when("/", {templateUrl:"/partials/schedule.html"});
-	});
+		$routeProvider.
+            when('/games', {templateUrl: 'partials/games.html', controller: 'GamesCtrl'}).
+            when('/schedule', {templateUrl: 'partials/schedule.html', controller: 'ScheduleCtrl'}).
+            when('/ranking', {templateUrl: 'partials/ranking.html', controller: 'RankingCtrl'}).
+            when('/home', {templateUrl: 'partials/home.html', controller: 'HomeCtrl'}).
+            otherwise({redirectTo: '/home'});
+    })
+//controllers per pagina//
+.controller('HomeCtrl', ['$scope', 'StateService', function ($scope, StateService) {
+        $scope.title = 'Home';
+        $scope.body = 'This is home body';
+
+        $scope.message = StateService.getMessage();
+
+        $scope.updateMessage = function (m) {
+            StateService.setMessage(m);
+        };
+    }])
+
+
+.controller('GamesCtrl', ['$scope', 'StateService', function ($scope, StateService) {
+        $scope.title = 'Games';
+        $scope.body = 'This is the games body';
+
+        $scope.message = StateService.getMessage();
+
+        $scope.updateMessage = function (m) {
+            StateService.setMessage(m);
+        };
+    }])
+
+
+.controller('ScheduleCtrl', ['$scope', 'StateService', function ($scope, StateService) {
+        $scope.title = 'Schedule';
+        $scope.body = 'This is the schedule body';
+
+        $scope.message = StateService.getMessage();
+
+        $scope.updateMessage = function (m) {
+            StateService.setMessage(m);
+        };
+    }])
+
+.controller('RankingCtrl', ['$scope', 'StateService', function ($scope, StateService) {
+        $scope.title = 'Ranking';
+        $scope.body = 'This is the ranking body';
+
+        $scope.message = StateService.getMessage();
+
+        $scope.updateMessage = function (m) {
+            StateService.setMessage(m);
+        };
+    }])
+
+    .factory('StateService', function () {
+        var message = 'Hello Message';
+        var getMessage = function() {
+            return message;
+        };
+        var setMessage = function(m) {
+            message = m;
+        };
+
+        return {
+            getMessage: getMessage,
+            setMessage: setMessage
+        }
+    });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 scoreApp.controller('ScheduleListController', function ScheduleListController($scope, $http) {
  
@@ -42,4 +123,9 @@ for (var i = 0; i < strippedData.length; i++) {
 
 
 });
+
+
+
+
+
 
