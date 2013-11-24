c9d18d14abac301c9d5112017016b7272066da3a
vanne89 <anne.vlaanderen@gmail.com>
2013-11-24 21:45:01 +0100

    sigh
    added rules,  started ranking

--- js/controllers.js
+++ js/controllers.js
@@ -1,7 +1,7 @@
- var accessToken = '82996312dc';
- var tournamentID = '19389';
+var accessToken = '82996312dc';
+var tournamentID = '19389';
 
-
+//routing
 var scoreApp = angular.module('scoreApp', []).
 	config(function($routeProvider){
 		$routeProvider.
@@ -9,18 +9,18 @@ var scoreApp = angular.module('scoreApp', []).
             when('/schedule', {templateUrl: 'partials/schedule.html', controller: 'ScheduleCtrl'}).
             when('/ranking', {templateUrl: 'partials/ranking.html', controller: 'RankingCtrl'}).
             when('/home', {templateUrl: 'partials/home.html', controller: 'HomeCtrl'}).
+            when('/rules', {templateUrl: 'partials/rules.html', controller: 'RulesCtrl'}).
             when('/game/:id', {templateUrl: 'partials/game.html', controller: 'GameCtrl'}).
             otherwise({redirectTo: '/home'});
     });
 
-//controllers per pagina//
- 
-    scoreApp.controller('HomeCtrl', ['$scope', function ($scope) {
+ //HOME CONTROLLER
+scoreApp.controller('HomeCtrl', ['$scope', function ($scope) {
         console.log("Has arrived at HomeController");
-
     }]);
 
-    scoreApp.controller('GameCtrl', ['$scope', '$routeParams', '$http', function ($scope, $routeParams, $http){
+//GAME CONTROLLER
+scoreApp.controller('GameCtrl', ['$scope', '$routeParams', '$http', function ($scope, $routeParams, $http){
     $scope.team_1 = "Loading..";
     $scope.team_2 = "Loading.."
     $scope.score_team_1 = "..";
@@ -65,13 +65,34 @@ var scoreApp = angular.module('scoreApp', []).
 };
     }]);
     
+//RANKING CONTROLLER
+scoreApp.controller('RankingCtrl', ['$scope', '$http', '$location', function ($scope, $http, $location) {
+        $scope.ranking = [];
 
-    scoreApp.controller('RankingCtrl', ['$scope', function ($scope) {
-        console.log("Has arrived at RankingController");
+        var jsonRequestURL = 'https://api.leaguevine.com/v1/games/?tournament_id='+ tournamentID +'&limit=50&access_token=' + accessToken;
+        $http({method: 'GET', url: jsonRequestURL}).
+        success(function(data, status, headers, config) {
+         var strippedData = data.objects;
+        for (var i = 0; i < strippedData.length; i++) {
+        var team = [];
+        team_name = strippedData[i];
+        team.wins = strippedData[i];
+        team.lost = strippedData[i];
+
+        console.log(ranking.number + "  " + ranking.team + "  " + ranking.wins + "/" + ranking.lost);
+
+        $scope.ranking.push(team);
+        };
+    }).
+        error(function(data, status, headers, config) {
+    // called asynchronously if an error occurs
+    // or server returns response with an error status.
+  });
 
     }]);
 
-     scoreApp.controller('GamesCtrl', ['$scope', '$http', '$location', function ($scope, $http, $location) {
+//GAMES CONTROLLER
+scoreApp.controller('GamesCtrl', ['$scope', '$http', '$location', function ($scope, $http, $location) {
         $scope.games = [];
 
         var jsonRequestURL = 'https://api.leaguevine.com/v1/games/?tournament_id='+ tournamentID +'&limit=50&access_token=' + accessToken;
@@ -88,16 +109,12 @@ var scoreApp = angular.module('scoreApp', []).
         game.score_team_2 = strippedData[i].team_2_score;
         game.pool = strippedData[i].pool.name;
 
-
         console.log(game.date + "  " + game.team_1 + " vs. " + game.team_2);
 
         $scope.games.push(game);
         };
-
-    
-
-  }).
-  error(function(data, status, headers, config) {
+    }).
+        error(function(data, status, headers, config) {
     // called asynchronously if an error occurs
     // or server returns response with an error status.
   });
@@ -106,9 +123,15 @@ var scoreApp = angular.module('scoreApp', []).
         $location.path('/game/' + id);
       }
       
+}]);
+
+//RULES CONTROLLER
+scoreApp.controller('RulesCtrl', ['$scope', function ($scope) {
+        console.log("Has arrived at RulesController");
     }]);
 
-    scoreApp.controller('ScheduleCtrl', function ScheduleListController($scope, $http) {
+//SCHEDULE CONTROLLER
+scoreApp.controller('ScheduleCtrl', function ScheduleListController($scope, $http) {
  
         $scope.loadingGif = '<img src="img/loading.GIF"> </img>';    
         $scope.games = [];
@@ -132,7 +155,7 @@ var scoreApp = angular.module('scoreApp', []).
 
         $scope.games.push(game);
 };
- console.log($scope.games);
+
 
 
   }).
@@ -140,10 +163,6 @@ var scoreApp = angular.module('scoreApp', []).
     // called asynchronously if an error occurs
     // or server returns response with an error status.
   });
-
-
-
-
 
 });
 
